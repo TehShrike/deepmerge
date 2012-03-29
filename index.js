a4583b18e3b6507a1267c698c4216673a84e063d
James Halliday <mail@substack.net>
2012-03-28 18:06:33 -0700

    immutable interface tests all pass

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 module.exports = function merge (target, src) {
+    var dst = {}
+    if (target && typeof target === 'object') {
+        Object.keys(target).forEach(function (key) {
+            dst[key] = target[key]
+        })
+    }
+    
     Object.keys(src).forEach(function (key) {
-        if (typeof src[key] !== 'object') {
-            target[key] = src[key]
+        if (typeof src[key] !== 'object' || !src[key]) {
+            dst[key] = src[key]
         }
         else {
-            if (!target[key] || typeof target[key] !== 'object') {
-                target[key] = {}
-            }
-            merge(target[key], src[key])
+            dst[key] = merge(target[key], src[key])
         }
     })
+    return dst
 }
