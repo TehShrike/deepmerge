b9fa2d0b075b328238b94467051714ab03fc53a2
vvo <v@fasterize.com>
2012-04-02 17:47:40 +0200

    Add array merge

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 module.exports = function merge (target, src) {
-    var dst = {}
-    if (target && typeof target === 'object') {
-        Object.keys(target).forEach(function (key) {
-            dst[key] = target[key]
+  var dst = {}
+  if (target && typeof target === 'object') {
+    if(Array.isArray(target)) {
+      dst = [].concat(target)
+        src.forEach(function(e, i){
+          if (target.indexOf(e) === -1) {
+            dst.push(e);
+          }
         })
+        return dst
+      } else {
+        Object.keys(target).forEach(function (key) {
+          dst[key] = target[key]
+      })
     }
-    
-    Object.keys(src).forEach(function (key) {
-        if (typeof src[key] !== 'object' || !src[key]) {
-            dst[key] = src[key]
-        }
-        else {
-            dst[key] = merge(target[key], src[key])
-        }
-    })
-    return dst
+  }
+
+  Object.keys(src).forEach(function (key) {
+    if (typeof src[key] !== 'object' || !src[key]) {
+      dst[key] = src[key]
+    }
+    else {
+      dst[key] = merge(target[key], src[key])
+    }
+  })
+  return dst
 }
