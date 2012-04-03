d2360972718604685639b80f3aaa52e9f5b55f0a
vvo <v@fasterize.com>
2012-04-03 17:12:54 +0200

    Set back indent to 4 spaces as @substack did, remove trailing `;`, `,` ..

--- test/merge.js
+++ test/merge.js
@@ -2,7 +2,7 @@ var merge = require('../')
 var test = require('tap').test
 
 test('add keys in target that do not exist at the root', function (t) {
-    var src = { key1 : 'value1', key2 : 'value2' }
+    var src = { key1: 'value1', key2: 'value2' }
     target = {}
 
     var res = merge(target, src)
@@ -13,13 +13,13 @@ test('add keys in target that do not exist at the root', function (t) {
 })
 
 test('merge existing simple keys in target at the roots', function (t) {
-    var src = { key1 : 'changed', key2 : 'value2' }
+    var src = { key1: 'changed', key2: 'value2' }
     var target = { key1: 'value1', key3: 'value3' }
 
     var expected = {
-        key1 : 'changed',
-        key2 : 'value2',
-        key3 : 'value3',
+        key1: 'changed',
+        key2: 'value2',
+        key3: 'value3'
     }
 
     t.deepEqual(target, { key1: 'value1', key3: 'value3' })
@@ -29,30 +29,30 @@ test('merge existing simple keys in target at the roots', function (t) {
 
 test('merge nested objects into target', function (t) {
     var src = {
-        key1 : {
-            subkey1 : 'changed',
-            subkey3 : 'added',
+        key1: {
+            subkey1: 'changed',
+            subkey3: 'added'
         }
     }
     var target = {
-        key1 : {
-            subkey1 : 'value1',
-            subkey2 : 'value2',
+        key1: {
+            subkey1: 'value1',
+            subkey2: 'value2'
         }
     }
 
     var expected = {
-        key1 : {
+        key1: {
             subkey1: 'changed',
             subkey2: 'value2',
-            subkey3: 'added',
+            subkey3: 'added'
         }
     }
 
     t.deepEqual(target, {
-        key1 : {
-            subkey1 : 'value1',
-            subkey2 : 'value2',
+        key1: {
+            subkey1: 'value1',
+            subkey2: 'value2'
         }
     })
     t.deepEqual(merge(target, src), expected)
@@ -61,48 +61,48 @@ test('merge nested objects into target', function (t) {
 
 test('replace simple key with nested object in target', function (t) {
     var src = {
-        key1 : {
-            subkey1 : 'subvalue1',
-            subkey2 : 'subvalue2',
+        key1: {
+            subkey1: 'subvalue1',
+            subkey2: 'subvalue2'
         }
     }
     var target = {
-        key1 : 'value1',
-        key2 : 'value2',
+        key1: 'value1',
+        key2: 'value2'
     }
 
     var expected = {
-        key1 : {
-            subkey1 : 'subvalue1',
-            subkey2 : 'subvalue2',
+        key1: {
+            subkey1: 'subvalue1',
+            subkey2: 'subvalue2'
         },
-        key2 : 'value2',
+        key2: 'value2'
     }
 
-    t.deepEqual(target, { key1 : 'value1', key2 : 'value2' })
+    t.deepEqual(target, { key1: 'value1', key2: 'value2' })
     t.deepEqual(merge(target, src), expected)
     t.end()
 })
 
 test('should replace object with simple key in target', function (t) {
-    var src = { key1 : 'value1' }
+    var src = { key1: 'value1' }
     var target = {
-        key1 : {
+        key1: {
             subkey1: 'subvalue1',
-            subkey2: 'subvalue2',
+            subkey2: 'subvalue2'
         },
-        key2 : 'value2',
+        key2: 'value2'
     }
 
-    var expected = { key1 : 'value1', key2 : 'value2' }
+    var expected = { key1: 'value1', key2: 'value2' }
 
     t.deepEqual(target, {
-        key1 : {
+        key1: {
             subkey1: 'subvalue1',
-            subkey2: 'subvalue2',
+            subkey2: 'subvalue2'
         },
-        key2 : 'value2',
-    });
+        key2: 'value2'
+    })
     t.deepEqual(merge(target, src), expected)
     t.end()
 })
@@ -120,12 +120,23 @@ test('should work on simple array', function (t) {
 })
 
 test('should work on array properties', function (t) {
-    var src = {key1 : ['one', 'three'], key2 : ['four']}
-    var target = {key1 : ['one', 'two']}
+    var src = {
+        key1: ['one', 'three'],
+        key2: ['four']
+    }
+    var target = {
+        key1: ['one', 'two']
+    }
 
-    var expected = {key1 : ['one', 'two', 'three'], key2 : ['four']}
+    var expected = {
+        key1: ['one', 'two', 'three'],
+        key2: ['four']
+    }
 
-    t.deepEqual(target, {key1 : ['one', 'two']})
+    t.deepEqual(target, {
+        key1: ['one', 'two']
+    })
+
     t.deepEqual(merge(target, src), expected)
     t.ok(Array.isArray(merge(target, src).key1))
     t.ok(Array.isArray(merge(target, src).key2))
@@ -133,12 +144,24 @@ test('should work on array properties', function (t) {
 })
 
 test('should work on array of objects', function (t) {
-    var src = [{key1: ['one', 'three'], key2: ['one']}, {key3: ['five']}]
-    var target = [{key1: ['one', 'two']}, {key3 : ['four']}]
+    var src = [
+        { key1: ['one', 'three'], key2: ['one'] },
+        { key3: ['five'] }
+    ]
+    var target = [
+        { key1: ['one', 'two'] },
+        { key3: ['four'] }
+    ]
 
-    var expected = [{key1: ['one', 'two', 'three'], key2: ['one']}, {key3: ['four', 'five']}]
+    var expected = [
+        { key1: ['one', 'two', 'three'], key2: ['one'] },
+        { key3: ['four', 'five'] }
+    ]
 
-    t.deepEqual(target, [{key1: ['one', 'two']}, {key3 : ['four']}])
+    t.deepEqual(target, [
+        { key1: ['one', 'two'] },
+        { key3: ['four'] }
+    ])
     t.deepEqual(merge(target, src), expected)
     t.ok(Array.isArray(merge(target, src)), 'result should be an array')
     t.ok(Array.isArray(merge(target, src)[0].key1), 'subkey should be an array too')
