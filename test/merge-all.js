52f108fc221b7eb1040ef1592ecbfe4707225d4e
Seva Zaikov <seva.zaikov@gmail.com>
2016-11-08 01:24:21 +0100

    fix test for clone check

--- test/merge-all.js
+++ test/merge-all.js
@@ -3,20 +3,24 @@ var test = require('tap').test
 
 test('throw error if first argument is not an array', function(t) {
   t.throws(merge.all.bind(null, { example: true }, { another: '2' }), Error)
+  t.end()
 })
 
 test('throw error if first argument is an array with least than two elements', function(t) {
   t.throws(merge.all.bind(null, [{ example: true }]), Error)
+  t.end()
 })
 
 test('execute correctly if options object were not passed', function(t) {
   var arrayToMerge = [{ example: true }, { another: '123' }]
   t.doesNotThrow(merge.all.bind(null, arrayToMerge))
+  t.end()
 })
 
 test('execute correctly if options object were passed', function(t) {
   var arrayToMerge = [{ example: true }, { another: '123' }]
   t.doesNotThrow(merge.all.bind(null, arrayToMerge, { clone:true }))
+  t.end()
 })
 
 test('invoke merge on every item in array should result with all props', function(t) {
@@ -31,16 +35,33 @@ test('invoke merge on every item in array should result with all props', functio
   t.ok(mergedObject.second === false)
   t.ok(mergedObject.third === 123)
   t.ok(mergedObject.fourth === 'some string')
+  t.end()
 })
 
-test('invoke merge on every item in array with clone should change object references', function(t) {
-  var firstObject = { link: { example: 123 } }
-  var secondObject = { link: { example: true } }
-  var thirdObject = { link: { example: 'string' } }
+test('invoke merge on every item in array with clone should clone all elements', function(t) {
+  var firstObject = { a: { d: 123 } }
+  var secondObject = { b: { e: true } }
+  var thirdObject = { c: { f: 'string' } }
 
-  var mergedObject = merge.all([firstObject, secondObject, thirdObject], { clone: true })
+  var mergedWithClone = merge.all([firstObject, secondObject, thirdObject], { clone: true })
 
-  t.ok(mergedObject.link !== firstObject.link)
-  t.ok(mergedObject.link !== secondObject.link)
-  t.ok(mergedObject.link !== thirdObject.link)
+  t.notEqual(mergedWithClone.a, firstObject.a)
+  t.notEqual(mergedWithClone.b, secondObject.b)
+  t.notEqual(mergedWithClone.c, thirdObject.c)
+
+  t.end()
+})
+
+test('invoke merge on every item in array without clone should not clone all elements', function(t) {
+  var firstObject = { a: { d: 123 } }
+  var secondObject = { b: { e: true } }
+  var thirdObject = { c: { f: 'string' } }
+
+  var mergedWithoutClone = merge.all([firstObject, secondObject, thirdObject])
+
+  t.equal(mergedWithoutClone.a, firstObject.a)
+  t.equal(mergedWithoutClone.b, secondObject.b)
+  t.equal(mergedWithoutClone.c, thirdObject.c)
+
+  t.end()
 })
