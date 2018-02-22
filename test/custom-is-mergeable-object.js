828f8c823277c07461b8cc8631f64af3b2a72af3
Josh Duff <me@JoshDuff.com>
2018-02-22 10:57:22 -0600

    Allow custom isMergeableObject functions
    Fixes #95
    Closes #93
    Fixes #90

---
+++ test/custom-is-mergeable-object.js
@@ -0,0 +1,36 @@
+var merge = require('../')
+var test = require('tap').test
+
+test('isMergeableObject function copying object over object', function(t) {
+	var src = { key: { isMergeable: false }, baz: 'yes' }
+	var target = { key: { foo: 'wat' }, baz: 'whatever' }
+
+	function isMergeableObject(object) {
+		return object && typeof value === 'object' && object.isMergeable !== false
+	}
+
+	var res = merge(target, src, {
+		isMergeableObject: isMergeableObject
+	})
+
+	t.deepEqual(res, { key: { isMergeable: false }, baz: 'yes' })
+	t.equal(res.key, src.key, 'Object has the same identity and was not cloned')
+	t.end()
+})
+
+test('isMergeableObject function copying object over nothing', function(t) {
+	var src = { key: { isMergeable: false, foo: 'bar' }, baz: 'yes' }
+	var target = { baz: 'whatever' }
+
+	function isMergeableObject(object) {
+		return object && typeof value === 'object' && object.isMergeable !== false
+	}
+
+	var res = merge(target, src, {
+		isMergeableObject: isMergeableObject
+	})
+
+	t.deepEqual(res, { key: { isMergeable: false, foo: 'bar' }, baz: 'yes' })
+	t.equal(res.key, src.key, 'Object has the same identity and was not cloned')
+	t.end()
+})
