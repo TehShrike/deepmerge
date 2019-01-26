7ee83915da2bbe52c97fcf434e95312617df2c44
radinio <oliver.radini@conted.ox.ac.uk>
2019-01-26 22:50:33 +0000

    updated type definitions

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,11 @@
 declare function deepmerge<T>(x: Partial<T>, y: Partial<T>, options?: deepmerge.Options): T;
 declare function deepmerge<T1, T2>(x: Partial<T1>, y: Partial<T2>, options?: deepmerge.Options): T1 & T2;
 
-declare interface CustomMerge<T1, T2> {
-  (x: Partial<T1>, y: Partial<T1 | T2>): T1 | T1 & T2;
-}
-
 declare namespace deepmerge {
 	export interface Options {
 		arrayMerge?(target: any[], source: any[], options?: Options): any[];
 		clone?: boolean;
-		customMerge?: (key: string, options?: Options) => (x: any, y: any) => any | undefined;
+		customMerge?: (key: string, options?: Options) => ((x: any, y: any) => any) | undefined;
 		isMergeableObject?(value: object): boolean;
 	}
 
