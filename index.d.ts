c5816342cb894aecbc4dc11ba301fa22c4eae4b2
Hunter <bryan.hunter@asurion.com>
2020-05-29 14:09:54 -0500

    typed deepmerge

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,21 @@
-declare function deepmerge<T>(x: Partial<T>, y: Partial<T>, options?: deepmerge.Options): T;
-declare function deepmerge<T1, T2>(x: Partial<T1>, y: Partial<T2>, options?: deepmerge.Options): T1 & T2;
+type ValueOfKey<T, K> = K extends keyof T ? T[K] : never;
+// Is object AND NOT ('never' or 'array')
+type IsObject<T> = [T] extends [never] ? false : T extends unknown[] ? false : [T] extends [object] ? true : false;
+type ShouldMergeProps<T1, T2> = IsObject<T1> extends false ? false : IsObject<T2> extends false ? false : true;
+type DeepMerge<T1, T2> = ShouldMergeProps<T1, T2> extends true
+    ? {
+          [K in keyof T1 | keyof T2]: DeepMerge<ValueOfKey<T1, K>, ValueOfKey<T2, K>>;
+      }
+    : T1 | T2;
+
+/**
+* ## JS Object Deep Merge
+* look more info in https://github.com/TehShrike/deepmerge
+* @param x the first obj
+* @param y the second obj
+* @param options deepmerge option
+*/
+declare function deepmerge<T1 extends object, T2 extends object>(x: T1, y: T2, options?: deepmerge.Options): DeepMerge<T1, T2>
 
 declare namespace deepmerge {
 	export interface Options {
