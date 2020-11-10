24b9aa90e4912f29cb80c625720e1ed0cf80d8b3
Rebecca Stevens <rebeccastevens@catalyst.net.nz>
2020-11-10 18:10:23 +1300

    fix: handle user functions in typings

--- types/index.d.ts
+++ types/index.d.ts
@@ -46,24 +46,53 @@ type DeepMergeObjectProps<
 	Options extends deepmerge.Options
 > = FlatternAlias<
 	{
-		-readonly [K in keyof T1]: Options["customMerge"] extends undefined
-			? DeepMerge<ValueOfKey<T1, K>, ValueOfKey<T2, K>, Options>
-			: ReturnType<NonNullable<Options["customMerge"]>> extends undefined
-			? DeepMerge<ValueOfKey<T1, K>, ValueOfKey<T2, K>, Options>
-			: ReturnType<
-					NonNullable<ReturnType<NonNullable<Options["customMerge"]>>>
-			  >;
+		-readonly [K in keyof T1]: DeepMergeProps<
+			ValueOfKey<T1, K>,
+			ValueOfKey<T2, K>,
+			Options
+		>;
 	} &
 		{
-			-readonly [K in keyof T2]: Options["customMerge"] extends undefined
-				? DeepMerge<ValueOfKey<T1, K>, ValueOfKey<T2, K>, Options>
-				: ReturnType<NonNullable<Options["customMerge"]>> extends undefined
-				? DeepMerge<ValueOfKey<T1, K>, ValueOfKey<T2, K>, Options>
-				: ReturnType<
-						NonNullable<ReturnType<NonNullable<Options["customMerge"]>>>
-				  >;
+			-readonly [K in keyof T2]: DeepMergeProps<
+				ValueOfKey<T1, K>,
+				ValueOfKey<T2, K>,
+				Options
+			>;
 		}
 >;
+
+type DeepMergeProps<T1, T2, Options extends deepmerge.Options> = GetOption<
+	Options,
+	"isMergeableObject"
+> extends undefined
+	? GetOption<Options, "customMerge"> extends undefined
+		? DeepMerge<T1, T2, Options>
+		: DeepMergePropsCustom<T1, T2, Options>
+	: MergeMaybeLeafs<T1, T2, Options>;
+
+type DeepMergePropsCustom<
+	T1,
+	T2,
+	Options extends deepmerge.Options
+> = ReturnType<NonNullable<GetOption<Options, "customMerge">>> extends undefined
+	? DeepMerge<T1, T2, Options>
+	: undefined extends ReturnType<NonNullable<GetOption<Options, "customMerge">>>
+	? IsArray<T1> extends true
+		? IsArray<T2> extends true
+			? DeepMergeArrays<T1, T2, Options>
+			: MergeLeafs<T1, T2>
+		: IsArray<T2> extends true
+		? MergeLeafs<T1, T2>
+		:
+				| DeepMerge<T1, T2, Options>
+				| ReturnType<
+						NonNullable<
+							ReturnType<NonNullable<GetOption<Options, "customMerge">>>
+						>
+				  >
+	: ReturnType<
+			NonNullable<ReturnType<NonNullable<GetOption<Options, "customMerge">>>>
+	  >;
 
 type DeepMergeArrays<
 	T1,
@@ -71,9 +100,9 @@ type DeepMergeArrays<
 	Options extends deepmerge.Options
 > = T1 extends readonly [...infer E1]
 	? T2 extends readonly [...infer E2]
-		? Options["arrayMerge"] extends undefined
+		? GetOption<Options, "arrayMerge"> extends undefined
 			? [...E1, ...E2]
-			: ReturnType<NonNullable<Options["arrayMerge"]>>
+			: ReturnType<NonNullable<GetOption<Options, "arrayMerge">>>
 		: never
 	: never;
 
@@ -85,11 +114,24 @@ type MergeLeafs<T1, T2> = Is<T2, never> extends true
 	? T1
 	: T2;
 
-type FlatternAlias<T> = T extends any
-	? {
-			[K in keyof T]: T[K];
-	  }
-	: never;
+type MergeMaybeLeafs<T1, T2, Options extends deepmerge.Options> = Is<
+	T2,
+	never
+> extends true
+	? T1
+	: Is<T1, never> extends true
+	? T2
+	: Is<T2, undefined> extends true
+	? T1
+	: Is<T1, undefined> extends true
+	? T2
+	: Is<T1, object> extends false
+	? DeepMerge<T1, T2, Options>
+	: Is<T2, object> extends false
+	? DeepMerge<T1, T2, Options>
+	: unknown;
+
+type FlatternAlias<T> = {} & { [P in keyof T]: T[P] };
 
 type ValueOfKey<T, K> = K extends keyof T ? T[K] : never;
 
@@ -102,6 +144,8 @@ type IsObjectOrArray<T> = Is<T, never> extends true ? false : Is<T, object>;
 type IsObject<T> = Is<T, ReadonlyArray<any>> extends true
 	? false
 	: IsObjectOrArray<T>;
+
+type IsArray<T> = Is<T, ReadonlyArray<any>>;
 
 type ShouldMergeObjects<T1, T2> = IsObject<T1> extends true
 	? IsObject<T2>
@@ -132,6 +176,11 @@ type DefaultOptions = {
 	customMerge: undefined;
 	isMergeableObject: undefined;
 };
+
+type GetOption<
+	O extends deepmerge.Options,
+	K extends keyof deepmerge.Options
+> = undefined extends O[K] ? DefaultOptions[K] : O[K];
 
 /**
  * Deeply merge two objects.
